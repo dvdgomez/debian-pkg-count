681264ea7d97c45687e46f250f04727725410eb6
David Gomez <david.13.gomez@gmail.com>
2022-04-28 20:14:42 -0700

    Package counting tool working

--- package_statistics.py
+++ package_statistics.py
@@ -1,4 +1,5 @@
 import sys
+import gzip
 import urllib.request
 from collections import Counter
 
@@ -13,7 +14,7 @@ def check_architecture(arch: str) -> bool:
     """
     return True if arch in options else False
 
-def download_contents_index(url: str) -> str:
+def download_contents_index(url: str, arch: str) -> list:
     """Download contents index.
 
     Args:
@@ -21,24 +22,35 @@ def download_contents_index(url: str) -> str:
     """
     # Try and download contents index, exit if download fails
     try:
-        urllib.request.urlretrieve(f"{url}", "contents_index")
-        # Open downloaded file and read
-        with open("contents_index", "r") as fp:
-            output = fp.read()
+        output = []
+        urllib.request.urlretrieve(f"{url}Contents-{arch}.gz", "contents_index.gz")
+        # Open downloaded .gz file and read
+        with gzip.open("contents_index.gz", "rb") as fp:
+            # Store package column of each line
+            for line in fp:
+                # Convert from bytes to string and skip filename column
+                line = line.decode("utf-8").split()[1:]
+                # Check if multiple packages per file
+                import pdb
+                if "," in line[0]:
+                    # Split comma separated packages line
+                    line = line[0].split(",")
+                    for pkg in line:
+                        # package name is last in path
+                        output.append(pkg.split("/")[-1])
+                else:
+                    # Only 1 package for file case
+                    output.append(line[0].split("/")[-1])
         return output
     except:
         sys.exit("Unable to download contents index!")
         
-def parse_and_count(content: str) -> list:
-    """Parse contents index and count files per package.
+def count_pkgs(pkgs: list) -> list:
+    """Count how many times a package was associated with a file in list
 
     Args:
-    content -- Content index text
+    content -- Content index package list per file
     """
-    # Split the text by comma delimiter
-    content = content.split(",")
-    # Prune filenames and only keep package names for file count
-    pkgs = ["".join(path.strip().split("/")[:-1]) for path in content]
     # Return list of top ten packages with most files
     return Counter(pkgs).most_common(10)
 
@@ -51,9 +63,10 @@ if __name__ == "__main__":
     architecture = sys.argv[1]
     # TODO: Check if architecture is valid otherwise exit
     # Download the respective contents index for the architecture
-    content = download_contents_index(URL)
+    content = download_contents_index(URL, architecture)
+    # TODO: Remove file after download
     # Parse the file output and count files per package
-    pkg_count = parse_and_count(content)
+    pkg_count = count_pkgs(content)
     # Output top ten packages with most files associated with them
     for index, pkg in enumerate(pkg_count):
-        print(f"{i}. {pkg[0]}  {pkg[1]}")
+        print(f"{index+1}. {pkg[0]}  {pkg[1]}")
